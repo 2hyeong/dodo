306f5a4b65b6cc31b69fa681a5c5b342398d5d51
2hyeong <doo.hyeongnim@gmail.com>
2023-02-22 20:02:22 +0900

    chore: add font-family on theme

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    fontFamily: {
+      notoSansBlack: ['NotoSansKR-Black'],
+      notoSansBold: ['NotoSansKR-Bold'],
+      notoSansLight: ['NotoSansKR-Light'],
+      notoSansMedium: ['NotoSansKR-Medium'],
+      notoSans: ['NotoSansKR-Regular'],
+      notoSansThin: ['NotoSansKR-Thin'],
+    },
+  },
+  plugins: [],
+};
